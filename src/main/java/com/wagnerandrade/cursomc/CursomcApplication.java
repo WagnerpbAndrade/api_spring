fa1cc7a3f5b372305e0749afc0a869526550f342
Wagner Andrade <wagnerpbandrade@gmail.com>
2019-12-29 18:39:55 -0300

    Criando operação de instanciação

--- src/main/java/com/wagnerandrade/cursomc/CursomcApplication.java
+++ src/main/java/com/wagnerandrade/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.wagnerandrade.cursomc;
 
+import com.wagnerandrade.cursomc.api.model.Categoria;
+import com.wagnerandrade.cursomc.api.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        this.repository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
